5a7bcfa1e640bde897410133ee23acda6a98d391
kambleps <125243027+kambleps@users.noreply.github.com>
2023-04-21 18:23:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 function insert_Row() {
     //Write your code here
-  let cell1 = newRow.insertCell(0);
-    let cell2 = newRow.insertCell(1);
-    cell1.textContent = 'New Cell1';
-    cell2.innerContent = 'New Cell2';
-    cell2.textContent = 'New Cell2';
+  var row = document.createElement('tr');
+	var td1 = document.createElement('td');
+	var td2 = document.createElement('td');
+
+	td1.textContent = 'New cell1';
+	td2.textContent = 'New cell2';
+
+	var table = document.getElementById('sampleTable');
+	table.appendChild(td1)
+	table.appendChild(td2)
+
+	row.appendChild(td1)
+	row.appendChild(td2)
+
+	row.appendChild(table)
   
 }
