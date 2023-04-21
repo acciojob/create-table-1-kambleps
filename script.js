1936757d287020e62488fab91cecdcd7fbe2c380
kambleps <125243027+kambleps@users.noreply.github.com>
2023-04-21 18:21:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 function insert_Row() {
     //Write your code here
-  
+  let cell1 = newRow.insertCell(0);
+    let cell2 = newRow.insertCell(1);
+    cell1.textContent = 'New Cell1';
+    cell2.innerContent = 'New Cell2';
+    cell2.textContent = 'New Cell2';
   
 }
